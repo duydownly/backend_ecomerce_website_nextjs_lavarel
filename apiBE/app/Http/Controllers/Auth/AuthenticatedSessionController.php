dc24a60fa56681ada5b6409e6a58e1e5e0039d8d
dizzytrn <dizzytrn@gmail.com>
2025-03-23 15:49:20 +0700

    apiBE

--- apiBE/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ apiBE/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,19 +7,30 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $user = Auth::user();
+
+        return response()->json([
+            'status' => 'success',
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'is_admin' => $user->is_admin,
+            ],
+        ]);
     }
 
     /**
